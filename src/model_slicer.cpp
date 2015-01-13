15361f1b58d306ff2ddc6ee1ffde2b6d6b2dcfbd
pshriwise <shriwise@wisc.edu>
2015-01-13 15:32:24 -0600

    Added plotutils plotting of the model.

--- src/model_slicer.cpp
+++ src/model_slicer.cpp
@@ -1,6 +1,7 @@
 
 #include <fstream>
 #include <iomanip>
+#include <plotter.h> //plotutils include
 
 #include "slicer.hpp"
 #include "moab/ProgOptions.hpp"
@@ -39,33 +40,82 @@ int main( int argc, char ** argv )
   std::vector< std::vector< std::vector< std::vector<double> > > >::iterator i; 
   std::vector< std::vector< std::vector<double> > >::iterator j; 
 
-  std::ofstream output; 
-  output.precision(6);
-  //output.set(std::ios::fixed);
-  //output.set(std::ios::showpoint);
-
- 
 
 
-  output.open("slicepnts.txt");
-
-  for( i = paths.begin(); i != paths.end(); i++ )
+   // set a Plotter parameter
+  PlotterParams params;
+  //params.setplparam ("PAGESIZE", (char *)"letter");
+  params.setplparam ("BITMAPSIZE", (char *)"400x400");
+     
+  XPlotter plotter(cin, cout, cerr, params); // declare Plotter
+  if (plotter.openpl () < 0)                  // open Plotter
     {
-      for ( j = (*i).begin(); j != (*i).end(); j++ )
+      cerr << "Couldn't open Plotter\n";
+      return 1;
+    }
+
+  //  plotter.fspace( -3, -5, 8, 5);
+  plotter.flinewidth (0.001);       // line thickness in user coordinates
+  plotter.fillcolorname("light blue");
+  plotter.pencolorname("black");
+  plotter.filltype(1);
+  plotter.erase();
+
+
+  //set index values
+  int graph_x, graph_y;
+  switch(axis){
+  case 0:
+    graph_x = 1;
+    graph_y = 2; 
+    break; 
+  case 1: 
+    graph_x = 0; 
+    graph_y = 2; 
+    break;
+  case 2:
+    graph_x = 0;
+    graph_y = 1; 
+    break;
+  }
+
+  double min_x=100000.0, max_x=-100000.0, min_y=100000.0, max_y=-100000.0;
+
+  for( i = paths.begin(); i != paths.end(); i++ ) //path loop
+    {
+      for ( j = (*i).begin(); j != (*i).end(); j++ ) //subpath loop
 	{
-	  for( unsigned int k = 0; k < (*j).size(); k++)
+	  for( unsigned int k = 0; k < (*j).size(); k++) //points loop
 	    {
-	    for( unsigned int ax = 0; ax <= 2; ax++)
-	      {
-		if ( ax != axis )
-		  output << (*j)[k][ax] << std::fixed << " ";
-	      }
-	    output << std::endl;
+	      double this_x = (*j)[k][graph_x];
+	      double this_y = (*j)[k][graph_y];
+	      if ( 0 == k ) plotter.fmove( this_x, this_y  );
+	      else plotter.fcont( this_x, this_y );
+
+	      if (min_x > this_x ) min_x = this_x;
+	      if (max_x < this_x ) max_x = this_x;
+
+	      if (min_y > this_y ) min_y = this_y;
+	      if (max_y < this_y ) max_y = this_y;
+
 	    }
-	  output << std::endl;
+	  plotter.endsubpath();
 	}
     }
 
-  output.close();
+  std::cout << min_x << std::endl;
+  std::cout << min_y << std::endl;
+  std::cout << max_x << std::endl;
+  std::cout << max_y << std::endl;
+
+  plotter.fspace( min_x, min_y, max_x, max_y); 
+
+  if (plotter.closepl () < 0)      // close Plotter
+    {
+      cerr << "Couldn't close Plotter\n";
+      return 1;
+    }
+  return 0;
+
 
 }
