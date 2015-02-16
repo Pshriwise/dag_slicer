12bd8889a5eed9a6c703d4ac884476c4937c77df
pshriwise <shriwise@wisc.edu>
2015-02-16 14:17:30 -0600

    Forgot to add the options header file.

---
+++ src/options.hpp
@@ -0,0 +1,15 @@
+#ifndef SLICER_OPTS_H
+#define SLICER_OPTS_H
+
+struct program_option_struct{
+  bool verbose;
+  bool debug;
+
+};
+
+extern struct program_option_struct opts;
+
+#define OPT_VERBOSE (opts.verbose || opts.debug)
+#define OPT_DEBUG   (opts.debug)
+
+#endif /* SLICER_OPTS_H */
