9efc079a2f390db3798e3e0d8cf40cc631c20e91
shriwise <pshriwise@gmail.com>
2017-05-14 12:24:48 -0500

    Some cleanup of the DagMC_Slicer class.

--- dag_slicer.cpp
+++ dag_slicer.cpp
@@ -1,9 +1,14 @@
 #include "dag_slicer.hpp"
 
-Dag_Slicer::Dag_Slicer( std::string file_to_slice, int ax, double coordinate, bool by_grp, bool ca)
-  : axis(ax), filename(file_to_slice), coord(coordinate), by_group(by_grp), verbose(false), debug(false), roam(false) {
+Dag_Slicer::Dag_Slicer( std::string file_to_slice,
+			int ax,
+			double coordinate,
+			bool by_grp,
+			bool ca)
+  : axis(ax), filename(file_to_slice), coord(coordinate), by_group(by_grp),
+    verbose(false), debug(false), roam(false) {
   //warning message if use has roaming point search enabled
-  roam_warning = "WARNING: Roaming for unmatched stitch points has been enabled. This method is intended for use as an attempt to slice unsealed meshes and isn't garaunteed to produce a valid mesh slice.";
+  _roam_warning = ROAM_WARNING
 }
 
   Dag_Slicer::~Dag_Slicer() { }
--- dag_slicer.hpp
+++ dag_slicer.hpp
@@ -5,32 +5,47 @@
 #include <string>
 #include <vector>
 
+#define ROAM_WARNING "WARNING: Roaming for unmatched stitch points has been \
+                      enabled. This feature is intended for use as an attempt to slice \
+                      unsealed meshes and isn't garaunteed to produce a valid results.";
+		      
 class Dag_Slicer{
   
 public:
-  Dag_Slicer(std::string file_to_slice, int ax, double coord, bool by_grp = false, bool ca = false);
+  // consructor
+  Dag_Slicer(std::string file_to_slice, // filename of the modle to slice
+	     int ax, // slice axis
+	     double coord, // coordinate of the slice
+	     bool by_grp = false, // slice by group (material) if true, by volume if false
+	     bool ca = false); // 
   ~Dag_Slicer();
-  //parameters
-  int axis;
-  std::string filename;
-  std::string roam_warning;
-  double coord;
-  bool by_group;
-  bool verbose;
-  bool debug;
-  bool roam;
-  //creation method
+
+  // slice creation method
   int create_slice();
+  
   //modify methods
   void rename_group(int group_global_id, std::string new_name);
   void write_file(std::string new_filename);
-  //data
+  
+  // output data
   std::vector<std::string> group_names;
   std::vector<double> dum_pnts;
   std::vector< std::vector<double> > slice_x_pnts;
   std::vector< std::vector<double> > slice_y_pnts; 
   std::vector<int> group_ids;
   std::vector< std::vector<int> > path_coding;
+
+  // slice parameters
+  std::string filename;
+  int axis;
+  double coord;
+  bool by_group;
+  bool verbose;
+  bool debug;
+  bool roam;
+
+  //other
+  std::string _roam_warning;
 };
 
 #endif
