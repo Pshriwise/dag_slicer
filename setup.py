b6cc28943b56d0ed1bf3f1efe04524e24ea9511c
shriwise <pshriwise@gmail.com>
2017-07-03 15:19:12 -0500

    Updating module locations.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ print moab_library_path
 libdirs = [moab_library_path]
 libs = ['MOAB']
 
-py_modules = ['dagmc_slice_tool','dagmc_slicer_gui','Dag_Slicer']
+py_modules = ['src/dagmc_slice_tool','src/dagmc_slicer_gui','src/Dag_Slicer']
 
 ext_modules = [
     Extension("_Dag_Slicer", ["src/dag_slicer_wrap.cxx","src/dag_slicer.cpp","src/slicer.cpp"], 
